e137fd014d9a2700ed9a5a38fdc9c33debc4ca30
Zetta330 <zetta330@outlook.com>
2026-04-03 19:45:29 -0400

    begun implementing saving

--- MikeyPomodorosApp/Settings.xaml.cs
+++ MikeyPomodorosApp/Settings.xaml.cs
@@ -31,11 +31,28 @@ namespace MikeyPomodorosApp
             StudyMinsBox.Text = ConfigManager._config.StudyTime.ToString();
             BreakMinsBox.Text = ConfigManager._config.BreakTime.ToString();
             LbMinsBox.Text = ConfigManager._config.LongBreakTime.ToString();
+            CyclesBeforeLongBreakBox.Text = ConfigManager._config.CyclesBeforeLongBreak.ToString();
+            AutoStartBox.IsChecked = ConfigManager._config.AutoStartTimer;
+            ShuffleBox.IsChecked = ConfigManager._config.Shuffle;
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void SaveExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(StudyMinsBox.Text, out var studytime))
+                ConfigManager._config.StudyTime = studytime;
+            if (int.TryParse(BreakMinsBox.Text, out var breaktime))
+                ConfigManager._config.BreakTime = breaktime;
+            if (int.TryParse(LbMinsBox.Text, out var lbtime))
+                ConfigManager._config.LongBreakTime = lbtime;
+            if (int.TryParse(CyclesBeforeLongBreakBox.Text, out var cblb))
+                ConfigManager._config.CyclesBeforeLongBreak = cblb;
+            ConfigManager._config.AutoStartTimer = AutoStartBox.IsChecked.Value;
+            ConfigManager._config.Shuffle = ShuffleBox.IsChecked.Value;
         }
     }
 }
